c0ab369e8e53d071adef8947811d00cb016c2a71
dungptm <dungptm2@vng.com.vn>
2025-04-11 10:34:02 +0700

    32-jdbc-autoconfig

--- lab/32-jdbc-autoconfig/src/main/java/rewards/RewardsRecipientProperties.java
+++ lab/32-jdbc-autoconfig/src/main/java/rewards/RewardsRecipientProperties.java
@@ -1,5 +1,7 @@
 package rewards;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
 // TODO-06 : Capture properties into a class using @ConfigurationProperties
 // - Note that application.properties file already contains the following properties
 //
@@ -19,6 +21,52 @@ package rewards;
 //   (3) Annotate this class with @Component
 // - Implement a new command line runner that displays the name of the rewards
 //   recipient when the application gets started
+@ConfigurationProperties(prefix = "rewards.recipient")
 public class RewardsRecipientProperties {
+    private String name;
+    private Integer age;
+    private String gender;
+    private String hobby;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "RewardsRecipientProperties{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
 }
